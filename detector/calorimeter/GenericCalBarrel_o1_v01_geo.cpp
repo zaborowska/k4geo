485c4b8474301f521c8ce325489fad67ed9e91eb
Anna Zaborowska <anna.zaborowska@cern.ch>
2024-03-19 17:02:57 +0100

    Use submodule part of readout and implement all active detector for CLD

--- detector/calorimeter/GenericCalBarrel_o1_v01_geo.cpp
+++ detector/calorimeter/GenericCalBarrel_o1_v01_geo.cpp
@@ -211,8 +211,9 @@ static Ref_t create_detector(Detector& theDetector, xml_h e, SensitiveDetector s
                 // Set region, limitset, and vis.
                 slice_vol.setAttributes(theDetector, x_slice.regionStr(), x_slice.limitsStr(), x_slice.visStr());
                 // slice PlacedVolume
-                layer_vol.placeVolume(slice_vol, Position(0, 0, slice_pos_z));
-                
+                PlacedVolume slice_phv = layer_vol.placeVolume(slice_vol, Position(0, 0, slice_pos_z));
+                slice_phv.addPhysVolID("submodule", slice_number);
+
                 // Increment Z position for next slice.
                 slice_pos_z += slice_thickness / 2;
                 // Increment slice number.
